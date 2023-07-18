f0dcd10fc122adaf7586407b458abfa47541a253
akbrsazish <sazishaliakbar@gmail.com>
2023-07-19 01:22:24 +0330

    add calculator buttons

---
+++ src/components/Calculator.js
@@ -0,0 +1,24 @@
+const InputField = () => <input type="text" className="inputField" />;
+const CalcButtons = () => (
+    <div className="calcContainer">
+      <button type="button" className="calcItem btn-number">AC</button>
+      <button type="button" className="calcItem btn-number">+/-</button>
+      <button type="button" className="calcItem btn-number">%</button>
+      <button type="button" className="calcItem btn-sign">/</button>
+      <button type="button" className="calcItem btn-number">7</button>
+      <button type="button" className="calcItem btn-number">8</button>
+      <button type="button" className="calcItem btn-number">9</button>
+      <button type="button" className="calcItem btn-sign">X</button>
+      <button type="button" className="calcItem btn-number">4</button>
+      <button type="button" className="calcItem btn-number">5</button>
+      <button type="button" className="calcItem btn-number">6</button>
+      <button type="button" className="calcItem btn-sign">-</button>
+      <button type="button" className="calcItem btn-number">1</button>
+      <button type="button" className="calcItem btn-number">2</button>
+      <button type="button" className="calcItem btn-number">3</button>
+      <button type="button" className="calcItem btn-sign">+</button>
+      <button type="button" className="calcItem btn-number btn-zero">0</button>
+      <button type="button" className="calcItem btn-number">.</button>
+      <button type="button" className="calcItem btn-sign">+</button>
+    </div>
+  );
